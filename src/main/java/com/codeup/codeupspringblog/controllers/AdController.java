2d383e5269e5fdb2d287a504174a56bf8f976233
Seth Martinez <sethchristopher.mtz@gmail.com>
2023-12-15 13:37:34 -0600

    still working the views

--- src/main/java/com/codeup/codeupspringblog/controllers/AdController.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/AdController.java
@@ -4,6 +4,8 @@ import com.codeup.codeupspringblog.models.Ad;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,5 +21,17 @@ public class AdController {
     public String showAllAds(Model model){
         model.addAttribute("ads", ads);
         return "/ads/index";
+    }
+
+
+    @GetMapping("/ads/create")
+    public String showCreateAdForm() {
+        return "/ads/create";
+    }
+
+    @PostMapping("/ads/create")
+    public String handleAdSubmission(@RequestParam(name="title") String title, @RequestParam(name="description") String description) {
+        ads.add(new Ad(title, description));
+        return "redirect:/ads";
     }
 }
